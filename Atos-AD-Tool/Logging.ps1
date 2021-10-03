ee71888257ac04e09aa021340bae93a351fc805d
ItsJesse <itsjesse777@gmail.com>
2021-10-03 19:55:50 -0400

    AD Pasword/unlock GUI
    AD Password reset/unlock tool for off-site HD

---
+++ Atos-AD-Tool/Logging.ps1
@@ -0,0 +1,29 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2018 v5.5.153
+	 Created on:   	8/20/2019 9:27 AM
+	 Created by:   	jrs
+	 Organization: 	
+	 Filename:     	Logging.ps1
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+[CmdletBinding()]
+param (
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[string]$Message,
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[ValidateSet('Information', 'Warning', 'Error')]
+	[string]$Severity = 'Information'
+)
+
+[pscustomobject]@{
+	Time = (Get-Date -f g)
+	Message = $Message
+	Severity = $Severity
+} | Export-Csv -Path "$PSScriptRoot\Logs\LogFile.csv" -Append -NoTypeInformation
---
+++ Atos-AD-Tool/Run-AD-Tool.ps1
@@ -0,0 +1,14 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2018 v5.5.153
+	 Created on:   	2/25/2019 1:27 PM
+	 Created by:   	jrs
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+& .\Atos-AD-Tool.exe
